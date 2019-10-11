f92d5ebf369e232baefd7e1825695c3eccc4d708
Pitrified <nobilipietro@gmail.com>
2019-10-11 20:59:58 +0200

    Add beginning of pole tutorial from pytorch

---
+++ pole/README.md
@@ -0,0 +1,1 @@
+A straightforward copy of https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
---
+++ pole/dq_pole.py
@@ -0,0 +1,328 @@
+import argparse
+import logging
+
+from random import seed
+from timeit import default_timer as timer
+from time import sleep
+
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+from utils import Transition
+from utils import ReplayMemory
+from utils import DQN
+
+
+def parse_arguments():
+    """Setup CLI interface
+    """
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        "-i",
+        "--path_input",
+        type=str,
+        default="hp.jpg",
+        help="path to input image to use",
+    )
+
+    parser.add_argument("-s", "--seed", type=int, default=-1, help="random seed to use")
+
+    # last line to parse the args
+    args = parser.parse_args()
+    return args
+
+
+def setup_logger(logLevel="DEBUG"):
+    """Setup logger that outputs to console for the module
+    """
+    logroot = logging.getLogger("c")
+    logroot.propagate = False
+    logroot.setLevel(logLevel)
+
+    module_console_handler = logging.StreamHandler()
+
+    #  log_format_module = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    #  log_format_module = "%(name)s - %(levelname)s: %(message)s"
+    #  log_format_module = '%(levelname)s: %(message)s'
+    log_format_module = "%(message)s"
+
+    formatter = logging.Formatter(log_format_module)
+    module_console_handler.setFormatter(formatter)
+
+    logroot.addHandler(module_console_handler)
+
+    logging.addLevelName(5, "TRACE")
+    # use it like this
+    # logroot.log(5, 'Exceedingly verbose debug')
+
+
+def run_dq_pole():
+    logg = logging.getLogger(f"c.{__name__}.run_dq_pole")
+    logg.debug(f"Start run_dq_pole")
+
+    env = gym.make("CartPole-v0").unwrapped
+
+    plt.ion()
+
+    # if gpu is to be used
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #  show_frame(env)
+
+    # hyperparameters
+    BATCH_SIZE = 128
+    GAMMA = 0.999
+    EPS_START = 0.9
+    EPS_END = 0.05
+    EPS_DECAY = 200
+    TARGET_UPDATE = 10
+
+    env.reset()
+    # Get screen size so that we can initialize layers correctly based on shape
+    # returned from AI gym. Typical dimensions at this point are close to 3x40x90
+    # which is the result of a clamped and down-scaled render buffer in get_screen()
+    init_screen = get_screen(env, device)
+    _, _, screen_height, screen_width = init_screen.shape
+
+    # Get number of actions from gym action space
+    n_actions = env.action_space.n
+
+    policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+    target_net = DQN(screen_height, screen_width, n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
+    target_net.eval()
+
+    optimizer = optim.RMSprop(policy_net.parameters())
+    memory = ReplayMemory(10000)
+
+    steps_done = 0
+    # XXX check that select_action lo aumenta davvero
+
+    # main training loop. At the beginning we reset the environment and
+    # initialize the state Tensor. Then, we sample an action, execute it,
+    # observe the next screen and the reward (always 1), and optimize our model
+    # once. When the episode ends (our model fails), we restart the loop.
+
+    num_episodes = 50
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        env.reset()
+        last_screen = get_screen(env, device)
+        current_screen = get_screen(env, device)
+        state = current_screen - last_screen
+        for t in count():
+            # Select and perform an action
+            action = select_action(state, n_actions, steps_done, device, EPS_START, EPS_END, EPS_DECAY)
+            _, reward, done, _ = env.step(action.item())
+            reward = torch.tensor([reward], device=device)
+
+            # Observe new state
+            last_screen = current_screen
+            current_screen = get_screen(env, device)
+            if not done:
+                next_state = current_screen - last_screen
+            else:
+                next_state = None
+
+            # Store the transition in memory
+            memory.push(state, action, next_state, reward)
+
+            # Move to the next state
+            state = next_state
+
+            # Perform one step of the optimization (on the target network)
+            optimize_model(BATCH_SIZE, memory)
+            if done:
+                episode_durations.append(t + 1)
+                plot_durations()
+                break
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode % TARGET_UPDATE == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+
+    print("Complete")
+    env.render()
+    env.close()
+    plt.ioff()
+    plt.show()
+
+    # gently close the env, avoid sys.meta_path undefined
+    env.close()
+
+
+def select_action(state, n_actions, steps_done, device, EPS_START, EPS_END, EPS_DECAY):
+    # will select an action accordingly to an epsilon greedy policy. Simply
+    # put, we’ll sometimes use our model for choosing the action, and sometimes
+    # we’ll just sample one uniformly. The probability of choosing a random
+    # action will start at EPS_START and will decay exponentially towards
+    # EPS_END. EPS_DECAY controls the rate of the decay
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(
+        -1.0 * steps_done / EPS_DECAY
+    )
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor(
+            [[random.randrange(n_actions)]], device=device, dtype=torch.long
+        )
+
+
+def optimize_model(BATCH_SIZE, memory):
+    # performs a single step of the optimization. It first samples a batch,
+    # concatenates all the tensors into a single one, computes Q(st,at) and
+    # V(st+1)=maxaQ(st+1,a), and combines them into our loss. By defition we
+    # set V(s)=0 if s is a terminal state. We also use a target network to
+    # compute V(st+1) for added stability. The target network has its weights
+    # kept frozen most of the time, but is updated with the policy network’s
+    # weights every so often. This is usually a set number of steps but we
+    # shall use episodes for simplicity.
+
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(
+        tuple(map(lambda s: s is not None, batch.next_state)),
+        device=device,
+        dtype=torch.uint8,
+    )
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = (
+        target_net(non_final_next_states).max(1)[0].detach()
+    )
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(
+        state_action_values, expected_state_action_values.unsqueeze(1)
+    )
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+def show_frame(env):
+    env.reset()
+    plt.figure()
+    plt.imshow(
+        get_screen(env, device).cpu().squeeze(0).permute(1, 2, 0).numpy(),
+        interpolation="none",
+    )
+    plt.title("Example extracted screen")
+    plt.show()
+    sleep(1)
+
+
+def get_cart_location(screen_width, env):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+
+def get_screen(env, device):
+    logg = logging.getLogger(f"c.{__name__}.get_screen")
+
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render(mode="rgb_array").transpose((2, 0, 1))
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height * 0.4) : int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width, env)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(
+            cart_location - view_width // 2, cart_location + view_width // 2
+        )
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+
+    # Resize, and add a batch dimension (BCHW)
+    resize = T.Compose(
+        [T.ToPILImage(), T.Resize(40, interpolation=Image.CUBIC), T.ToTensor()]
+    )
+    return resize(screen).unsqueeze(0).to(device)
+
+
+def main():
+    setup_logger()
+
+    args = parse_arguments()
+
+    # setup seed value
+    if args.seed == -1:
+        myseed = 1
+        myseed = int(timer() * 1e9 % 2 ** 32)
+    else:
+        myseed = args.seed
+    seed(myseed)
+    np.random.seed(myseed)
+
+    path_input = args.path_input
+
+    recap = f"python3 dq_pole.py"
+    recap += f" --path_input {path_input}"
+    recap += f" --seed {myseed}"
+
+    logmain = logging.getLogger(f"{__name__}.main")
+    logmain.info(recap)
+
+    run_dq_pole()
+
+
+if __name__ == "__main__":
+    main()
